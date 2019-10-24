81348e25db045dfb6c0c7e1f49eeb118fa5eb980
javierg <javierg@amcoonline.net>
2019-10-24 09:30:15 -0700

    keep spacing

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+	value: true
 });
 
 var _pouchdb = require('pouchdb');
@@ -11,23 +11,23 @@ var _pouchdb2 = _interopRequireDefault(_pouchdb);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function plugin(plugin) {
-  _pouchdb2.default.plugin(plugin);
+	_pouchdb2.default.plugin(plugin);
 }
 
 function install(Vue) {
-  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+	var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-  if (!options.name) throw new Error('vuejs-pouchdb → error → please set db name!');
-  var dbName = options.name;
-  delete options.name;
-  var db = new _pouchdb2.default(dbName, options);
+	if (!options.name) throw new Error('vuejs-pouchdb → error → please set db name!');
+	dbName = options.name;
+	delete options.name;
+	var db = new _pouchdb2.default(dbName, options);
 
-  Vue.pouch = db;
-  Vue.prototype.$pouch = db;
+	Vue.pouch = db;
+	Vue.prototype.$pouch = db;
 }
 
 exports.default = {
-  install: install,
-  plugin: plugin,
-  pouchdb: _pouchdb2.default
+	install: install,
+	plugin: plugin,
+	pouchdb: _pouchdb2.default
 };
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import pouchdb from 'pouchdb'
 
 function plugin (plugin) {
- pouchdb.plugin(plugin)
+	pouchdb.plugin(plugin)
 }
 
 function install (Vue, options = {}) {
-  if(!options.name) throw new Error('vuejs-pouchdb → error → please set db name!')
-  let dbName = options.name
-  delete options.name
-  let db = new pouchdb(dbName, options)
+	if(!options.name) throw new Error('vuejs-pouchdb → error → please set db name!')
+	dbName = options.name
+	delete options.name
+	let db = new pouchdb(dbName, options)
 
-  Vue.pouch = db
-  Vue.prototype.$pouch = db
+	Vue.pouch = db
+	Vue.prototype.$pouch = db
 }
 
 export default {
-  install,
-  plugin,
-  pouchdb
+	install,
+	plugin,
+	pouchdb
 }
